4ada5fb3b0d82c0f2a35aa69774a1bd315c286ef
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-12-10 15:39:35 -0800

    Create search.sh

---
+++ search.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ -e test1.testtt ] || [ -e test1.mp3 ]]
+then
+     echo "the script did not delete the files"
+     exit 0;
+else
+     echo "sucess!  the script deleted the files!"
+fi
