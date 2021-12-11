cceb795acdab7a7de659f458e182118300b3d375
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-12-11 13:08:58 -0800

    Update final.sh

--- final.sh
+++ final.sh
@@ -9,6 +9,6 @@ fi
 filename="$1"
 cp $filename $filename.bak
 
-vim -u NONE -r ."$filename".swp -c 'w! '"$filename"'' -c 'wq!'
+vim -u NONE -r "$filename".swp -c 'w! '"$filename"'' -c 'wq!'
 
 sdiff $filename $filename.bak
