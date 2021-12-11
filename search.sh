bf185b9d3469c93579450c512b12b30baa6e1ee2
nic-instruction <nicole.bade@seattlecolleges.edu>
2021-12-11 10:18:28 -0800

    Delete search.sh

--- search.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -e test1.testtt ] || [ -e test1.mp3 ]
-then
-     echo "the script did not delete the files"
-     exit 0;
-else
-     echo "sucess!  the script deleted the files!"
-fi
